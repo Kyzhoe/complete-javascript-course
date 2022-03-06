8ac9e07765fae90d15a6206635969c1a26a57b90
Kyzhoe <96223287+Kyzhoe@users.noreply.github.com>
2022-03-06 14:24:55 -0800

    Update challenge1.js

--- 02-Fundamentals-Part-2/starter/challenge1.js
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -2,8 +2,8 @@
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-const avgDolphins = calcAverage(85, 54, 41);
-const avgKoalas = calcAverage(23, 34, 27);
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
 console.log(avgDolphins, avgKoalas);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
@@ -17,4 +17,12 @@ const checkWinner = function (avgDolphins, avgKoalas) {
         console.log(`Koalas scored higher, but did not win. (${avgKoalas} vs. ${avgDolphins})`);
     } else console.log("???");
 }
+checkWinner(avgDolphins, avgKoalas);
+
+checkWinner(574, 111);
+
+// Test 2
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+console.log(avgDolphins, avgKoalas);
 checkWinner(avgDolphins, avgKoalas);
