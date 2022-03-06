bd90343d69f75554fe34d3f0a37645b2f6047216
Kyzhoe <96223287+Kyzhoe@users.noreply.github.com>
2022-03-06 14:12:31 -0800

    Challenge 1 and other stuffs

---
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins > avgKoalas && avgDolphins > (avgKoalas * 2)) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgDolphins > avgKoalas && avgDolphins < (avgKoalas * 2)) {
+        console.log(`Dolphins scored higher, but did not win. (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgDolphins < avgKoalas && (avgDolphins * 2) < avgKoalas) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+    } else if (avgDolphins < avgKoalas && (avgDolphins * 2) > avgKoalas) {
+        console.log(`Koalas scored higher, but did not win. (${avgKoalas} vs. ${avgDolphins})`);
+    } else console.log("???");
+}
+checkWinner(avgDolphins, avgKoalas);
