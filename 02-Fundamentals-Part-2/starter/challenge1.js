7f51b4ef16f65e05e636ed1e797acaa6bfec810b
Kyzhoe <96223287+Kyzhoe@users.noreply.github.com>
2022-03-06 14:14:18 -0800

    Update challenge1.js

--- 02-Fundamentals-Part-2/starter/challenge1.js
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
+
 const avgDolphins = calcAverage(85, 54, 41);
 const avgKoalas = calcAverage(23, 34, 27);
+
 function checkWinner(avgDolphins, avgKoalas) {
     if (avgDolphins > avgKoalas && avgDolphins > (avgKoalas * 2)) {
         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
