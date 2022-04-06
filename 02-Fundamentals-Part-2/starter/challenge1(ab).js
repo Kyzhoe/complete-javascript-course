af7b857a8edea60fc9413c33a25d4771fa52a232
Kyzhoe <96223287+Kyzhoe@users.noreply.github.com>
2022-04-06 12:22:59 -0700

    arrays

--- 02-Fundamentals-Part-2/starter/challenge1(ab).js
+++ 02-Fundamentals-Part-2/starter/challenge1(ab).js
@@ -1,13 +1,19 @@
 'use strict';
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
-let avgDolphins;
-let avgKoalas;
+let scoreDolphins = calcAverage(85, 54, 41);
+let scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
 
-let checkWinner = function (avgDolphins, avgKoalas) {
-    if (avgDolphins > avgKoalas && avgDolphins >= (avgKoalas * 2))
-        console.log(`The Dolphins win ${avgDolphins} vs. ${avgKoalas}!`)
-    if (avgKoalas > avgDolphins && avgKoalas >= (avgDolphins * 2))
-        console.log(`The Koalas win ${avgKoalas} vs. ${avgDolphins}!`)
-    else console.log("???")
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins > 2 * avgKoalas) {
+        console.log(`The Dolphins win ${avgDolphins} vs. ${avgKoalas}!`);
+    }
+    else if (avgKoalas > 2 * avgDolphins) {
+        console.log(`The Koalas win ${avgKoalas} vs. ${avgDolphins}!`);
+    } else {
+        console.log('No team wins...');
+    }
 }
+checkWinner(scoreDolphins, scoreKoalas);
