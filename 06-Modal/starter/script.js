21aafc69ce0a0e76526a3a3ec110d644e8e4b1ef
Kyzhoe <96223287+Kyzhoe@users.noreply.github.com>
2022-04-10 13:27:47 -0700

    Update script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,12 +6,11 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const CloseModal = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -19,5 +18,12 @@ const CloseModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', CloseModal);
-overlay.addEventListener('click', CloseModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
